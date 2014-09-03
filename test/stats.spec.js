4a4a19b4b6bf5481b30f7cf6a5e7cc7b4f950362
Tomas Stropus <h.guzas@gmail.com>
2014-09-04 00:50:07 +0300

    added stats api

---
+++ test/stats.spec.js
@@ -0,0 +1,34 @@
+var config = require('./config');
+var api = require('../lib/lolapi')(config.apiKey);
+var expect = require('chai').expect;
+
+describe('api', function () {
+
+  describe('#stats', function () {
+    var summonerId = 71054;
+
+    it('should return summoners ranked stats', function (done) {
+      api.Stats.getRanked(summonerId, {}, function (error, result) {
+        expect(error).to.not.be.ok;
+        expect(result).to.be.ok;
+        expect(result).to.be.an('object');
+        expect(result.modifyDate).to.be.ok;
+        expect(result.summonerId).to.equal(summonerId);
+        done();
+      });
+    });
+
+    it('should return summoners stat summary', function (done) {
+      api.Stats.getSummary(summonerId, {}, function (error, result) {
+        expect(error).to.not.be.ok;
+        expect(result).to.be.ok;
+        expect(result).to.be.an('object');
+        expect(result.playerStatSummaries).to.be.ok;
+        expect(result.summonerId).to.equal(summonerId);
+        done();
+      });
+    });
+
+  });
+
+});
