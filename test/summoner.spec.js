0ce4075b8fc4cfcddbe9046e3fa0228a2964c274
Tomas Stropus <h.guzas@gmail.com>
2014-09-04 10:13:32 +0300

    added summoner and team apis

--- test/summoner.spec.js
+++ test/summoner.spec.js
@@ -2,12 +2,110 @@ var config = require('./config');
 var api = require('../lib/lolapi')(config.apiKey);
 var expect = require('chai').expect;
 
-xdescribe('api', function () {
+describe('api', function () {
 
-  describe('summoner', function () {
+  describe('#summoner', function () {
+    var summonerNames = ['wickd', 'froggen'];
+    var summonerIds = [71500, 19531813];
 
-    it('should test', function () {
-      expect(1).to.be.a('number');
+    it('should retrieve summoner by name', function (done) {
+      api.Summoner.getByName(summonerNames[0], {}, function (error, result) {
+        expect(error).to.not.be.ok;
+        expect(result).to.be.ok;
+        expect(result).to.be.an('object');
+        expect(result[summonerNames[0]]).to.be.ok;
+        expect(result[summonerNames[0]]).to.be.an('object');
+        expect(result[summonerNames[0]].name.toLowerCase()).to.equal(summonerNames[0]);
+        done();
+      });
+    });
+
+    it('should retrieve multiple summoners by name', function (done) {
+      api.Summoner.getByName(summonerNames, {}, function (error, result) {
+        expect(error).to.not.be.ok;
+        expect(result).to.be.ok;
+        expect(result).to.be.an('object');
+        expect(result[summonerNames[0]]).to.be.ok;
+        expect(result[summonerNames[0]]).to.be.an('object');
+        expect(summonerNames).to.include(result[summonerNames[0]].name.toLowerCase());
+        expect(result[summonerNames[1]]).to.be.ok;
+        expect(result[summonerNames[1]]).to.be.an('object');
+        expect(summonerNames).to.include(result[summonerNames[1]].name.toLowerCase());
+        done();
+      });
+    });
+
+    it('should retrieve summoners name', function (done) {
+      api.Summoner.getName(summonerIds[0], {}, function (error, result) {
+        expect(error).to.not.be.ok;
+        expect(result).to.be.ok;
+        expect(result).to.be.an('object');
+        expect(result[summonerIds[0]].toLowerCase()).to.equal(summonerNames[0]);
+        done();
+      });
+    });
+
+    it('should retrieve multiple summoners names', function (done) {
+      api.Summoner.getName(summonerIds, {}, function (error, result) {
+        expect(error).to.not.be.ok;
+        expect(result).to.be.ok;
+        expect(result).to.be.an('object');
+        expect(result[summonerIds[0]]).to.be.ok;
+        expect(result[summonerIds[1]]).to.be.ok;
+        expect(summonerNames).to.include(result[summonerIds[0]].toLowerCase());
+        expect(summonerNames).to.include(result[summonerIds[1]].toLowerCase());
+        done();
+      });
+    });
+
+    it('should retrieve summoners runes', function (done) {
+      api.Summoner.getRunes(summonerIds[0], {}, function (error, result) {
+        expect(error).to.not.be.ok;
+        expect(result).to.be.ok;
+        expect(result).to.be.an('object');
+        expect(result[summonerIds[0]]).to.ok;
+        expect(result[summonerIds[0]]).to.be.an('object');
+        expect(result[summonerIds[0]].summonerId).to.equal(summonerIds[0]);
+        done();
+      });
+    });
+
+    it('should retrieve multiple summoners runes', function (done) {
+      api.Summoner.getRunes(summonerIds, {}, function (error, result) {
+        expect(error).to.not.be.ok;
+        expect(result).to.be.ok;
+        expect(result).to.be.an('object');
+        expect(result[summonerIds[0]]).to.be.ok;
+        expect(result[summonerIds[1]]).to.be.ok;
+        expect(summonerIds).to.include(result[summonerIds[0]].summonerId);
+        expect(summonerIds).to.include(result[summonerIds[1]].summonerId);
+        done();
+      });
+    });
+
+    it('should retrieve summoners masteries', function (done) {
+      api.Summoner.getMasteries(summonerIds[0], {}, function (error, result) {
+        expect(error).to.not.be.ok;
+        expect(result).to.be.ok;
+        expect(result).to.be.an('object');
+        expect(result[summonerIds[0]]).to.ok;
+        expect(result[summonerIds[0]]).to.be.an('object');
+        expect(result[summonerIds[0]].summonerId).to.equal(summonerIds[0]);
+        done();
+      });
+    });
+
+    it('should retrieve multiple summoners masteries', function (done) {
+      api.Summoner.getMasteries(summonerIds, {}, function (error, result) {
+        expect(error).to.not.be.ok;
+        expect(result).to.be.ok;
+        expect(result).to.be.an('object');
+        expect(result[summonerIds[0]]).to.be.ok;
+        expect(result[summonerIds[1]]).to.be.ok;
+        expect(summonerIds).to.include(result[summonerIds[0]].summonerId);
+        expect(summonerIds).to.include(result[summonerIds[1]].summonerId);
+        done();
+      });
     });
 
   });
---
+++ test/team.js
@@ -0,0 +1,33 @@
+var config = require('./config');
+var api = require('../lib/lolapi')(config.apiKey);
+var expect = require('chai').expect;
+
+describe('api', function () {
+
+  describe('#team', function () {
+    var summonerIds = [71500, 19531813];
+
+    it('should retrieve summoners teams', function (done) {
+      api.Team.getBySummonerId(summonerIds[0], {}, function (error, result) {
+        expect(error).to.not.be.ok;
+        expect(result).to.be.ok;
+        expect(result).to.be.an('object');
+        expect(result[summonerIds[0]]).to.be.ok;
+        done();
+      });
+    });
+
+    it('should retrieve multiple summoners teams', function (done) {
+      api.Team.getBySummonerId(summonerIds, {}, function (error, result) {
+        expect(error).to.not.be.ok;
+        expect(result).to.be.ok;
+        expect(result).to.be.an('object');
+        expect(result[summonerIds[0]]).to.be.ok;
+        expect(result[summonerIds[1]]).to.be.ok;
+        done();
+      });
+    });
+
+  });
+
+});
