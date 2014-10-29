d8902a2e26671ffa4a16dfb6bf1314126ebc5978
Tomas Stropus <h.guzas@gmail.com>
2014-10-29 23:22:07 +0200

    Updated TravisCI config

--- test/config.js
+++ test/config.js
@@ -1,7 +1,9 @@
-if (!API_KEY) {
+var apiKey = process.env.API_KEY || API_KEY;
+
+if (apiKey) {
   throw new Error("Please set an API key in test/config.js before running tests.");
 }
 
 module.exports = {
-  apiKey: API_KEY
+  apiKey: apiKey
 };
